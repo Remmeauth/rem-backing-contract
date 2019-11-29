1202b263ddbcdf2614bfbd9ba61159c196ea0ac7
Ember <vlader112@gmail.com>
2019-11-29 20:39:21 +0200

    added distribute, setaccounts, removeacc actions

--- contract/include/guardian.reward.hpp
+++ contract/include/guardian.reward.hpp
@@ -1,18 +1,99 @@
 #pragma once
 
+#include <eosio/asset.hpp>
+#include <eosio/singleton.hpp>
 #include <eosio/eosio.hpp>
 
 namespace eosio {
 
-    class [[eosio::contract("rem.reward")]] reward : public contract {
-    public:
+   class [[eosio::contract("guardian.reward")]] reward : public contract {
+   public:
 
-        using contract::contract;
+      reward(name receiver, name code,  datastream<const char*> ds);
 
-        [[eosio::action]]
-        void distribute();
+      /**
+       * Get balance method.
+       *
+       * @details Get the balance for a token `sym_code` created by `token_contract_account` account,
+       * for account `owner`.
+       *
+       * @param token_contract_account - the token creator account,
+       * @param owner - the account for which the token balance is returned,
+       * @param sym_code - the token for which the balance is returned.
+       */
+      [[eosio::action]]
+      void distribute();
 
-    private:
-    };
-    /** @}*/ // end of @defgroup eosioauth rem.oracle
+      /**
+       * Get balance method.
+       *
+       * @details Get the balance for a token `sym_code` created by `token_contract_account` account,
+       * for account `owner`.
+       *
+       * @param token_contract_account - the token creator account,
+       * @param owner - the account for which the token balance is returned,
+       * @param sym_code - the token for which the balance is returned.
+       */
+      [[eosio::action]]
+      void setaccounts(const std::vector<name> &guardian, const std::vector<double> &reward_pct);
+
+      /**
+       * Get balance method.
+       *
+       * @details Get the balance for a token `sym_code` created by `token_contract_account` account,
+       * for account `owner`.
+       *
+       * @param token_contract_account - the token creator account,
+       * @param owner - the account for which the token balance is returned,
+       * @param sym_code - the token for which the balance is returned.
+       */
+      [[eosio::action]]
+      void removeacc(const name &guardian);
+
+      /**
+       * Get balance method.
+       *
+       * @details Get the balance for a token `sym_code` created by `token_contract_account` account,
+       * for account `owner`.
+       *
+       * @param token_contract_account - the token creator account,
+       * @param owner - the account for which the token balance is returned,
+       * @param sym_code - the token for which the balance is returned.
+       */
+      static asset get_balance( const name& token_contract_account, const name& owner, const symbol& sym )
+      {
+          accounts accountstable( token_contract_account, owner.value );
+          const auto& ac = accountstable.get( sym.code().raw(), "balance not found" );
+          return ac.balance;
+      }
+
+      using create_action = action_wrapper<"distribute"_n, &reward::distribute>;
+   private:
+      static constexpr name system_account = "rem"_n;
+      static constexpr name token_account  = "rem.token"_n;
+      static constexpr symbol core_symbol  = symbol(symbol_code("REM"), 4);
+
+      struct [[eosio::table]] guardians_data {
+         std::map<name, double> reward_distribution;
+
+         // explicit serialization macro is not necessary, used here only to improve compilation time
+         EOSLIB_SERIALIZE( guardians_data, (reward_distribution))
+      };
+
+      struct [[eosio::table]] account {
+         asset    balance;
+
+         uint64_t primary_key()const { return balance.symbol.code().raw(); }
+      };
+
+      typedef eosio::multi_index< "accounts"_n,  account >  accounts;
+      typedef singleton< "rewarddist"_n,  guardians_data >  guardians_idx;
+
+      guardians_idx guardians_tbl;
+      guardians_data guardians;
+
+      void claim_rewards(const name &owner);
+      void transfer_tokens(const name &from, const name &to, const asset &quantity, const std::string &memo);
+   };
+   /** @}*/ // end of @defgroup rem.reward
 } /// namespace eosio
--- contract/src/guardian.reward.cpp
+++ contract/src/guardian.reward.cpp
@@ -1,14 +1,68 @@
-/**
- *  @copyright defined in eos/LICENSE.txt
- */
-
 #include <guardian.reward.hpp>
 
 namespace eosio {
 
-    void reward::distribute()
-    {}
+   reward::reward(name receiver, name code,  datastream<const char*> ds)
+   :contract(receiver, code, ds),
+    guardians_tbl(get_self(), get_self().value)
+    {
+      guardians = guardians_tbl.exists() ? guardians_tbl.get() : guardians_data{};
+    }
+
+   void reward::distribute()
+   {
+      claim_rewards( get_self() );
+
+      asset contract_balance = get_balance(token_account, get_self(), core_symbol);
+      check(contract_balance.amount > 0, "contract balance should be a positive");
+
+      for (const auto &guardian: guardians.reward_distribution) {
+         asset quantity = { static_cast<int64_t>(contract_balance.amount * guardian.second), core_symbol };
+         if (quantity.amount > 0) {
+            transfer_tokens(get_self(), guardian.first, quantity, "remme distribution reward");
+         }
+      }
+   }
+
+   void reward::setaccounts(const std::vector<name> &guardian, const std::vector<double> &reward_pct)
+   {
+      require_auth( get_self() );
+
+      for (size_t i = 0; i < guardian.size(); ++i) {
+         guardians.reward_distribution[guardian.at(i)] = reward_pct.at(i);
+      }
+   guardians_tbl.set(guardians, same_payer);
+   }
+
+   void reward::removeacc(const name &guardian)
+   {
+      require_auth( get_self() );
+
+      auto it = guardians.reward_distribution.find(guardian);
+      check(it != guardians.reward_distribution.end(), "guardian not found");
+
+      guardians.reward_distribution.erase(guardian);
+      guardians_tbl.set(guardians, same_payer);
+   }
+
+   void reward::claim_rewards(const name &owner)
+   {
+      action(
+         permission_level{owner, "active"_n},
+         "rem"_n, "claimrewards"_n,
+         std::make_tuple(owner)
+      ).send();
+   }
+
+   void reward::transfer_tokens(const name &from, const name &to, const asset &quantity, const std::string &memo)
+   {
+      action(
+         permission_level{from, "active"_n},
+         token_account, "transfer"_n,
+         std::make_tuple(from, to, memo)
+      ).send();
+   }
 } /// namespace eosio
 
-EOSIO_DISPATCH( eosio::reward, (distribute) )
+EOSIO_DISPATCH( eosio::reward, (distribute)(setaccounts)(removeacc) )
 
