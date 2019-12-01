92902b2c260810235e238413c8568b4ae426f0c3
Vlad Pavlichek <ember.toon@protonmail.com>
2019-12-01 18:23:41 +0200

    Added distrewards, setaccounts, removeacc actions (#2)
    * added distrewards, setaccounts, removeacc actions
    
    * refactor names in contract

---
+++ rem.backing/src/rem.backing.cpp
@@ -0,0 +1,67 @@
+#include <rem.backing.hpp>
+
+namespace eosio {
+
+   backing::backing(name receiver, name code,  datastream<const char*> ds)
+   :contract(receiver, code, ds),
+    rewards_dist_tbl(get_self(), get_self().value)
+    {
+      rewards_dist = rewards_dist_tbl.exists() ? rewards_dist_tbl.get() : rewards_data{};
+    }
+
+   void backing::distrewards()
+   {
+      claim_rewards( get_self() );
+
+      asset contract_balance = get_balance(token_account, get_self(), core_symbol);
+      check(contract_balance.amount > 0, "contract balance should be a positive");
+
+      for (const auto &guardian: rewards_dist.reward_distribution) {
+         asset quantity = { static_cast<int64_t>(contract_balance.amount * guardian.second), core_symbol };
+         if (quantity.amount > 0) {
+            delegatebw(get_self(), guardian.first, quantity, true);
+         }
+      }
+   }
+
+   void backing::setaccounts(const std::vector<name> &accounts, const std::vector<double> &reward_pct)
+   {
+      require_auth( get_self() );
+
+      for (size_t i = 0; i < accounts.size(); ++i) {
+         rewards_dist.reward_distribution[accounts.at(i)] = reward_pct.at(i);
+      }
+   rewards_dist_tbl.set(rewards_dist, same_payer);
+   }
+
+   void backing::removeacc(const name &account)
+   {
+      require_auth( get_self() );
+
+      auto it = rewards_dist.reward_distribution.find(account);
+      check(it != rewards_dist.reward_distribution.end(), "account not found");
+
+      rewards_dist.reward_distribution.erase(account);
+      rewards_dist_tbl.set(rewards_dist, same_payer);
+   }
+
+   void backing::claim_rewards(const name &owner)
+   {
+      action(
+         permission_level{owner, "active"_n},
+         system_account, "claimrewards"_n,
+         std::make_tuple(owner)
+      ).send();
+   }
+
+   void backing::delegatebw(const name& from, const name& receiver, const asset& stake_quantity, bool transfer)
+   {
+      action(
+         permission_level{from, "active"_n},
+         system_account, "delegatebw"_n,
+         std::make_tuple(from, receiver, stake_quantity, transfer)
+      ).send();
+   }
+} /// namespace eosio
+
+EOSIO_DISPATCH( eosio::backing, (distrewards)(setaccounts)(removeacc) )
